d60992f17e1c87abc075263303aabb6b7d29f0ff
@FyreFox360 <jmge5833@gmail.com>
2023-09-24 15:31:56 -0600

    Update generate-pdf.service.ts

--- src/generate-pdf/generate-pdf.service.ts
+++ src/generate-pdf/generate-pdf.service.ts
@@ -5,7 +5,6 @@ const PDFDocument1 = require('pdfkit-table');
 
 @Injectable()
 export class GeneratePdfService {
-  // Identificacion del logger para el metodo de la clase
   private readonly logger = new Logger(GeneratePdfService.name);
 
   async generatePDF(id: number, res: any): Promise<void> {
@@ -15,61 +14,91 @@ export class GeneratePdfService {
       const doc = new PDFDocument1({
         size: 'LETTER',
         bufferPages: true,
-        // le decimos que la primera pag no sea por defecto
-        autoFirstPage: false,
+        autoFirstPage: true, // Hacer que la primera página se agregue automáticamente
       });
 
-      let pageNumber = 0;
-      // agregar paginacion y encabezado
-      doc.on('pagedAdded', () => {
-        pageNumber++;
-        if (pageNumber > 1) {
-          doc.image(
-            join(process.cwd(), 'Public/logo.png'),
-            doc.page.width - 100,
-            5,
-            { fit: [45, 45], align: 'center' },
-          );
-          doc
-            .moveTo(50, 55)
-            .lineTo(doc.page.width - 50, 55)
-            .stroke();
-        }
+      // Contiene el contenido final del documento PDF
+      const buffer = [] as Buffer[];
 
-        let bottom = doc.page.margins.bottom;
+      // Espacio vertical entre elementos
+      const verticalSpacing = 5;
 
-        doc.page.margins.bottom = 0;
-        doc.text(
-          'Pag. ' + pageNumber,
-          (doc.page.width - 100) * 0.5,
-          doc.page.height - 50,
-          {
-            width: 100,
-            align: 'center',
-            lineBreak: false,
-          },
+      // Función para agregar encabezado en la primera página
+      const addHeader = () => {
+        // Agrega "Veterinaria Mistun" a la izquierda del documento y al mismo nivel
+        doc.text('Veterinaria Mistun', 60, 65, {
+          width: doc.page.width - 250, // Ajusta la posición horizontal según sea necesario
+          align: 'left', // Alinea el texto a la izquierda
+        });
+
+        // Imagen en el lado derecho del encabezado
+        doc.image(
+          join(process.cwd(), 'Public/logo.png'),
+          doc.page.width - 100,
+          33, // Ajusta la posición vertical para que esté al mismo nivel que el texto
+          { fit: [45, 45], align: 'center' },
         );
-        doc.page.margings.bottom = bottom;
+
+        // Línea horizontal debajo del encabezado
+        doc
+          .moveTo(50, 80) // Ajusta la posición vertical
+          .lineTo(doc.page.width - 50, 80) // Ajusta la posición vertical
+          .stroke();
+
+        // Espacio vertical entre elementos
+        doc.moveDown(verticalSpacing);
+      };
+
+      // Función para agregar los campos solicitados
+      const addFields = () => {
+        // Alinea "Nombre:" y agrega la variable cambiante
+        doc.text('Nombre: {variable cambiante}', { continued: true });
+        doc.moveDown(); // Salto de línea
+        // Alinea "Especie:"
+        doc.text('Especie:', { continued: true });
+        doc.moveDown(); // Salto de línea
+        // Alinea "Raza:"
+        doc.text('Raza:', { continued: true });
+        doc.moveDown(); // Salto de línea
+        // Alinea "Sexo:"
+        doc.text('Sexo:', { continued: true });
+        doc.moveDown(); // Salto de línea
+        // Alinea "Edad:"
+        doc.text('Edad:', { continued: true });
+        doc.moveDown(); // Salto de línea
+        // Alinea "Peso:"
+        doc.text('Peso:', { continued: true });
+        doc.moveDown(); // Salto de línea
+        // Alinea "Fecha de Nacimiento:"
+        doc.text('Fecha de Nacimiento:', { continued: true });
+        doc.moveDown(); // Salto de línea
+        // Alinea "Identificación del microchip:"
+        doc.text('Identificación del microchip:');
+      };
+
+      // Manejo de eventos para recopilar el contenido del PDF
+      doc.on('data', buffer.push.bind(buffer));
+      doc.on('end', () => {
+        const pdfData = Buffer.concat(buffer);
+        resolve(pdfData);
       });
-      doc.addPage();
-      doc.addPage();
-      doc.image(
-        join(process.cwd(), 'Public/logo.png'),
-        doc.page.width / 2 - 100,
-        150,
-        { width: 200 },
-      );
-      doc.text('', 0, 400);
-      doc.font('Helvetica-Bold').fontSize(24);
-      doc.text('DEV LATAM ', {
-        width: doc.page.width,
+
+      // Contenido del PDF, como logo y texto
+      addHeader();
+
+      // Alinea "Constancia de Salud" al centro
+      doc.text('Constancia de Salud', {
+        width: doc.page.width - 100,
         align: 'center',
       });
 
+      // Agrega los campos sin espacio adicional
+      addFields();
+
       const table = {
         title: 'Tabla de datos',
-        subtittle: 'Subtitulo de la tabla',
-        headers: ['id', 'nombre'],
+        subtitle: 'Subtítulo de la tabla',
+        headers: ['ID', 'Nombre'],
         rows: [
           ['1', 'Gutierrez'],
           ['2', 'Francisco'],
@@ -77,20 +106,10 @@ export class GeneratePdfService {
       };
 
       doc.table(table, { columnSize: [150, 300] });
-      doc.text('Hello world!', 100, 100);
-      doc.moveDown();
-      doc.text('ASI QUEDARÁ EL PDF ME FALTA ESTILO');
-
-      //contiene el contenido final del documento pdf
-      const buffer = [] as Buffer[];
-      doc.on('data', buffer.push.bind(buffer));
-      doc.on('end', () => {
-        const pdfData = Buffer.concat(buffer);
-        resolve(pdfData);
-      });
 
       doc.end();
     });
+
     const responsePDF = formatDocument(pdfBuffer, res);
 
     // Envía el PDF como respuesta
