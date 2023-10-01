4ec8a4f44a9444db011a27f5eaf0c9547a65e78c
@FyreFox360 <jmge5833@gmail.com>
2023-10-01 15:50:24 -0600

    Update generate-pdf.controller.ts
    olvide quitar el comment de la proteccion de rutas.

--- src/generate-pdf/generate-pdf.controller.ts
+++ src/generate-pdf/generate-pdf.controller.ts
@@ -1,21 +1,21 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Res, Body, Param } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Res, UseGuards, Body, Param } from '@nestjs/common';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { Response } from 'express';
 import { GeneratePdfService } from './generate-pdf.service';
 import { CreateDocumentInput } from './dto/input/create-constancia.input';
 
 // Proteccion de controllador
-// import RoleGuard from '../auth/guards/role.guard';
-// import { UserRole } from '../users/dto/enum/role.enum';
+import RoleGuard from '../auth/guards/role.guard';
+import { UserRole } from '../users/dto/enum/role.enum';
 
 @ApiTags('Generate-PDF')
 @Controller('generate-pdf')
-// @UseGuards(RoleGuard(UserRole.ADMIN))
+@UseGuards(RoleGuard(UserRole.ADMIN))
 export class GeneratePdfController {
   constructor(private readonly generatePdfService: GeneratePdfService) {}
 
-  // @ApiBearerAuth()
+  @ApiBearerAuth()
   @Get('/constancia-salud/:idPet')
   async createPDF(
     @Param('idPet') idPet: number,
