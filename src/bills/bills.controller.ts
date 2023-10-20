81d1362068bd9205e129e3e50fb54c2626ec6023
fabio248 <fabioflores021@gmail.com>
2023-10-19 21:37:07 -0600

    feat: add find bills functionality

--- src/bills/bills.controller.ts
+++ src/bills/bills.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  Query,
+  Param,
+} from '@nestjs/common';
 import { BillsService } from './services/bills.service';
 import { CreateBillInput } from './dto/input/create-bill.input';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import RoleGuard from '../auth/guards/role.guard';
 import { UserRole } from '../users/dto/enum/role.enum';
 import { BillResponse } from './dto/response/bill.response';
+import { GenericWithSearchArgs } from '../shared/args/generic-with-search.args';
+import { FindAllBillsResponse } from './dto/response/find-all-bills.response';
 
 @ApiTags('Bills')
 @UseGuards(RoleGuard(UserRole.ADMIN))
@@ -20,12 +30,14 @@ export class BillsController {
   }
 
   @Get()
-  findAll() {
-    return this.billsService.findAll();
+  findAll(
+    @Query() args?: GenericWithSearchArgs,
+  ): Promise<FindAllBillsResponse> {
+    return this.billsService.findAll(args);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<BillResponse> {
     return this.billsService.findOne(+id);
   }
 }
--- src/bills/services/bills.service.ts
+++ src/bills/services/bills.service.ts
@@ -4,6 +4,10 @@ import { BillsDetailsService } from './bills-details.service';
 import { PrismaService } from '../../database/database.service';
 import { plainToInstance } from 'class-transformer';
 import { BillResponse } from '../dto/response/bill.response';
+import { BillNotFoundException } from '../exception/bill-not-found.exception';
+import { GenericWithSearchArgs } from '../../shared/args/generic-with-search.args';
+import { getPaginationParams } from '../../shared/helper/pagination-params.helper';
+import { FindAllBillsResponse } from '../dto/response/find-all-bills.response';
 
 @Injectable()
 export class BillsService {
@@ -48,14 +52,53 @@ export class BillsService {
       },
     );
 
-    return plainToInstance(BillResponse, bill);
+    return plainToInstance(BillResponse, bill, {
+      excludeExtraneousValues: true,
+    });
   }
 
-  findAll() {
-    return `This action returns all bills`;
+  async findAll(args: GenericWithSearchArgs): Promise<FindAllBillsResponse> {
+    this.logger.log('Retrieve all bills');
+    const { page, limit } = args;
+
+    const [bills, totalItems] = await Promise.all([
+      this.prisma.bill.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        include: {
+          client: true,
+          billsDetails: true,
+        },
+      }),
+      this.prisma.bill.count(),
+    ]);
+
+    const paginationParams = getPaginationParams(totalItems, page, limit);
+
+    return {
+      data: plainToInstance(BillResponse, bills, {
+        excludeExtraneousValues: true,
+      }),
+      ...paginationParams,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} bill`;
+  async findOne(id: number): Promise<BillResponse> {
+    this.logger.log('Retrieve bill');
+    const bill = await this.prisma.bill.findUnique({
+      where: { id },
+      include: {
+        client: true,
+        billsDetails: true,
+      },
+    });
+
+    if (!bill) {
+      throw new BillNotFoundException(id);
+    }
+
+    return plainToInstance(BillResponse, bill, {
+      excludeExtraneousValues: true,
+    });
   }
 }
